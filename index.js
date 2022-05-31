beeff5e64805f9bc9ada67a093209e9c33c9015e
elias <eliassoto.ingenieria@gmail.com>
2022-05-31 12:15:21 -0500

    2comit

--- index.js
+++ index.js
@@ -7,6 +7,69 @@ app.get('/',(req,res)=>{
   res.send('hola mi server en express');
 })
 
+app.get('/nueva-ruta',(req,res)=>{
+
+  res.send('hola soy una nueva ruta');
+})
+
+app.get('/productos',(req,res)=>{
+  res.json([
+    {
+      name:'producto 1',
+      price: 1000
+    },
+    {
+      name:'producto 2',
+      price: 2000
+    }
+
+
+  ]);
+})
+
+app.get('/productos/:id',(req,res)=>{
+  //de todos los parametros solo me interesa id
+  const {id} = req.params;
+  res.json({
+id,
+name:'producto 2',
+price:2000
+  })
+  })
+
+  //para realizar query o filtros
+app.get('/users',(req,res)=>{
+  const {limit,offset}=req.query;
+if (limit && offset) {
+  res.json({
+    limit,
+    offset
+  })
+
+} else {
+res.send('no hay parametros');
+}
+
+})
+
+app.get('/categoria/:categoriaid/productos/:productoid',(req,res)=>{
+  const {categoriaid,productoid} = req.params;
+  res.json({
+    categoriaid,
+    productoid,
+      })
+
+})
+
+app.get('/usuario/:usuarioid/productos/:productoid',(req,res)=>{
+  const {usuarioid,productoid} = req.params;
+  res.json({
+    usuarioid,
+    productoid,
+      })
+
+})
+
 app.listen(port,()=>{
 
 console.log('mi port'+ port)
